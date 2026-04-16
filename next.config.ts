c3d2f1de0d3cf63fcaf372ddf9fae5af6e8da965
aquesadaaNSG <109000677+aquesadaaNSG@users.noreply.github.com>
2026-04-15 19:35:34 -0600

    Add project dependencies and Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
